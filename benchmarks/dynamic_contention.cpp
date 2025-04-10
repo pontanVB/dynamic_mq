a5474c5aac46ad46249d870cbb9dbfea8f6f09ac
pontanVB <pontus.w.m@gmail.com>
2025-04-10 23:06:17 +0200

    changed default timeout to 0

--- benchmarks/dynamic_contention.cpp
+++ benchmarks/dynamic_contention.cpp
@@ -59,7 +59,7 @@ struct Settings {
     long long batch_size = 1 << 12;
     int seed = 1;
     int affinity = 6;
-    int timeout_s = 12;
+    int timeout_s = 0;
     int sleep_us = 0;
     std::deque<std::pair<int,std::chrono::seconds>> thread_intervals;
     std::filesystem::path interval_file = "thread_intervals.txt";
